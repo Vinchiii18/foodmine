d42aa21e08755324808147af26bacdf2c43dc3c8
Alvin Consular <alvin.consular@accenture.com>
2024-05-10 02:11:36 +0800

    modified:   frontend/package-lock.json 	modified:   frontend/package.json 	modified:   frontend/src/app/app.module.ts 	modified:   frontend/src/app/components/pages/food-page/food-page.component.html 	modified:   frontend/src/app/components/pages/home/home.component.html

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,7 +7,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/partials/header/header.component';
 import { HomeComponent } from './components/pages/home/home.component';
-import { RatingModule } from 'ng-starrating';
 import { SearchComponent } from './components/partials/search/search.component';
 import { TagsComponent } from './components/partials/tags/tags.component';
 import { FoodPageComponent } from './components/pages/food-page/food-page.component';
@@ -35,8 +34,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    AppRoutingModule,
-    RatingModule,
+    AppRoutingModule,    
     HttpClientModule,
     ReactiveFormsModule,
     ToastrModule.forRoot({
